6a9d9f27d7edad3fbc60b375085da75972122ef6
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2022-05-13 15:43:58 +0600

    added website link to readme file

--- src/Pages/Home/UpcomingEvents/SliderClassCompo/SliderClassCompo.js
+++ src/Pages/Home/UpcomingEvents/SliderClassCompo/SliderClassCompo.js
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
-import Slider from 'react-slick';
-import SingleEventCard from '../../SingleEventCard/SingleEventCard';
+import React, { Component } from "react";
+import Slider from "react-slick";
+import SingleEventCard from "../../SingleEventCard/SingleEventCard";
 
-export default class SliderClassCompo  extends Component  {
-    
-    constructor(props) {
-        super(props);
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
-      }
-      next() {
-        this.slider.slickNext();
-      }
-      previous() {
-        this.slider.slickPrev();
-      }
+export default class SliderClassCompo extends Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  previous() {
+    this.slider.slickPrev();
+  }
 
-    render(){
+  render() {
+    const settings = {
+      autoplay: true,
+      dots: false,
+      infinite: true,
+      speed: 800,
+      slidesToShow: 1,
+      pauseOnHover: false,
+      slidesToScroll: 1,
+    };
 
-        const settings = {
-            autoplay: true,
-            dots: false,
-            infinite: true,
-            speed: 800,
-            slidesToShow: 1,
-            pauseOnHover: false,
-            slidesToScroll: 1
-          };
+    return (
+      <div>
+        <div className="my-5">
+          <i
+            onClick={this.previous}
+            class="fas fa-long-arrow-alt-left left-arrow me-4"
+          ></i>
+          <i
+            onClick={this.next}
+            class="fas fa-long-arrow-alt-right left-arrow"
+          ></i>
+        </div>
 
-        return (
-            <div>
-                    <div className="my-5">
-                        <i onClick={this.previous} class="fas fa-long-arrow-alt-left left-arrow me-4"></i>
-                        <i onClick={this.next} class="fas fa-long-arrow-alt-right left-arrow"></i>
-                    </div>
-                    
-                    <Slider ref={c => (this.slider = c)} {...settings}>
-                        {
-                            this.props.event.map( p => <SingleEventCard key={p.id} data={p}/>)
-                        }
-                    </Slider>
-            </div>
-        );
-    }
-
-};
+        <Slider ref={(c) => (this.slider = c)} {...settings}>
+          {this.props.event.map((p) => (
+            <SingleEventCard key={p.id} data={p} />
+          ))}
+        </Slider>
+      </div>
+    );
+  }
+}
